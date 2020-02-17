299c50d6ee5edadfb4e85a37925183b01707cdcd
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-02-17 17:03:22 -0500

    complete project

---
+++ app.py
@@ -0,0 +1,29 @@
+from tornado.web import Application, RequestHandler
+from tornado.ioloop import IOLoop
+from loan_decision import LoanDecision
+import json
+import tornado.httpserver
+import os
+
+class LoanDecisionApi(RequestHandler):
+    def get(self):
+        self.write({'message': 'hello world'})
+
+    def post(self):
+        body = json.loads(self.request.body)
+        decision = LoanDecision.validate_loan(requested_amount=body['requested_amount'])
+
+        self.write({'decision':decision})
+
+def make_app():
+    urls = [
+    ("/validate_loan", LoanDecisionApi)
+    ]
+    return Application(urls, debug=True)
+
+if __name__ == '__main__':
+    app = make_app()
+    http_server = tornado.httpserver.HTTPServer(app)
+    port = int(os.environ.get("PORT", 5000))
+    http_server.listen(port)
+    IOLoop.instance().start()
